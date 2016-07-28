0cfe1f00f27d49b7284175fbb62cb8936ee22187
Stephan Richter <github@keawe.de>
2016-07-28 02:20:33 +0200

    trying preliminary implementation of user service

---
+++ user/login.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!isset($_GET['username'])) die(NULL);
+if (!isset($_GET['password'])) die(NULL);
+$username = $_GET['username'];
+$password = $_GET['password']);
+if ($username == 'srichter'){
+	die('asdfghjkl',date('U'));
+}
+die(NULL);
+
+?>
