dd856289d31bc79b9dd20459f164f3c7b7e03e5b
Stephan Richter <github@keawe.de>
2016-07-28 05:44:33 +0200

    added user.menu

--- user/menu.php
+++ user/menu.php
@@ -5,6 +5,17 @@ include 'bootstrap.php';
 if ($token == null) die(NULL);
 
 $perms = request('permission','get?token='.$token);
-print_r($perms);
+if (!is_array($perms)) die(NULL);
 
+if (!is_array($perms['user'])) die(NULL);
 
+$permission_entries = array('add'  => array('add' => 'add new user'),
+			    'list' => array('list' => 'list all users'));
+
+$entries = array();
+foreach ($perms['user'] as $permission){
+	if (array_key_exists($permission,$permission_entries)){
+		$entries[] = $permission_entries[$permission];
+	}
+}
+die(json_encode($entries));
