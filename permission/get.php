ac457854a8c748b615d1a3cf32f9ca3c33111828
Stephan Richter <github@keawe.de>
2016-07-28 03:35:53 +0200

    working on permissions

--- permission/get.php
+++ permission/get.php
@@ -1,14 +1,21 @@
 <?php
 
-session_start();
+function getUrl($path){
+	$proto = 'http';
+	if ($_SERVER['HTTPS'] == 'on') $proto.='s'
+	$proto.='://';
+	
+	$host = $_SERVER['HTTP_HOST'];
+	$script = dirname($_SERVER['SCRIPT_NAME']);
+	
+	return $proto.$host.$script.'/'.$path;  
+}
 
 if (!isset($_GET['token'])) die(NULL);
 $token = $_GET['token'];
+if ($token == null) die(NULL);
 
-if ($token == 'asdfghjkl'){
-	$permission='user.add';
-	die($permission);
-}
+die(getURL('user/validate?token='.$token'));
 die(NULL);
 
 ?>
