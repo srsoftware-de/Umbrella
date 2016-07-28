f06155d8f06e6791feaf7639b2d9fe3e976df739
Stephan Richter <github@keawe.de>
2016-07-28 04:26:42 +0200

    rewritten request function

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$services[]=array();
+$services['user']='https://eldorado.keawe.de:816/microservices/user/';
+
+?>
--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
 
-function getUrl($path){
-	$proto = 'http';
-	if ($_SERVER['HTTPS'] == 'on') $proto.='s';
-	$proto.='://';
+include 'config.php';
 
-	$host = $_SERVER['HTTP_HOST'];
-	$script = dirname($_SERVER['SCRIPT_NAME']);
+$services = array('user','permission');
 
-	return $proto.$host.$script.'/'.$path;
+function getUrl($service,$path){
+	global $services;
+	return $services[$service].$path;
 }
 
-function request($path){
-	$url = getUrl($path);
+function request($service,$path){
+	$url = getUrl($service,$path);
 	echo $url.'<br/>';
 	return file_get_contents($url);
 }
@@ -21,11 +19,20 @@ $token = null;
 if (isset($_GET['token'])) $token = $_GET['token'];
 
 if (isset($_GET['username']) && isset($_GET['password'])){
-	$token = request('user/login?username='.$_GET['username'].'&password='.$_GET['password']);
+	$token = request('user','login?username='.$_GET['username'].'&password='.$_GET['password']);
 }
 
 if ($token == null){
 	include('user/form/login.php');
 	die();
 }
-die(request('permission/get?token='.$token));
+
+function getMenuEntries($service){
+	$response = request($service,'menu');
+}
+
+$menu_entries = array();
+foreach ($services as $service){
+	$menu_entries[$service] = getMenuEntries($service);
+}
+die(print_r($menu_entries);
