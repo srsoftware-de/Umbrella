9a52410355e0feff24705f9c61cb3fa3bf2e69c6
Stephan Richter <github@keawe.de>
2016-07-28 03:24:32 +0200

    improving glue

--- index.php
+++ index.php
@@ -12,14 +12,21 @@ function getUrl($path){
 }
 
 session_start();
+	
+if (isset($_GET['username']) && isset($_GET['password'])){
+	$url = getUrl('user/login?username='.$_GET['username'].'&password='.$_GET['password']);
+	$token = file_get_contents($url);
+	if ($token == null) {
+		unset($_SESSION['token']);
+	} else {
+		$_SESSION['token'] = $token;
+	}
+}
 
 if (!isset($_SESSION['token'])){
-	if (isset($_GET['username']) && isset($_GET['password'])){
-		$url = getUrl('user/login?username='.$_GET['username'].'&password='.$_GET['password']);
-		$token = file_get_contents($url);
-		die($token);
-	}
 	include('user/form/login.php');
 	die();
 }
-die();
+
+$url = getUrl('permission/get?token='.$_SESSION['token']);
+die(file_get_contents($url));
