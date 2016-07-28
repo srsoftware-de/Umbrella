666e72b86bbef9193c19354bff125533477dbe8e
Stephan Richter <github@keawe.de>
2016-07-28 02:31:50 +0200

    implemented preliminary version of checkValidity

--- user/login.php
+++ user/login.php
@@ -1,11 +1,11 @@
 <?php
-
 if (!isset($_GET['username'])) die(NULL);
 if (!isset($_GET['password'])) die(NULL);
 $username = $_GET['username'];
-$password = $_GET['password']);
+$password = $_GET['password'];
 if ($username == 'srichter'){
-	die('asdfghjkl',date('U'));
+	$token = 'asdfghjkl';
+	die($token);
 }
 die(NULL);
 
