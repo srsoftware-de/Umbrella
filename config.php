15dfa666c757d0f3c55d835b20a35d07158731ff
Stephan Richter <github@keawe.de>
2016-07-28 04:43:03 +0200

    extended config

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 
 $services[]=array();
 $services['user']='https://eldorado.keawe.de:816/microservices/user/';
-
+$services['permission']='https://eldorado.keawe.de:816/microservices/permission/';
 ?>
