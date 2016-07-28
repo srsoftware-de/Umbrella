3dd5bc0a6760d8b18c3770ade13760f4a6ed6b88
Stephan Richter <github@keawe.de>
2016-07-28 03:13:25 +0200

    working on glue

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
 
+function getUrl($path){
+	$proto = 'http';
+	if ($_SERVER['HTTPS'] == 'on') $proto.='s';
+	$proto.='://';
+
+	$host = $_SERVER['HTTP_HOST'];
+	$script = dirname($_SERVER['SCRIPT_NAME']);
+
+	return $proto.$host.$script.'/'.$path;
+}
+
 session_start();
 
 if (!isset($_SESSION['token'])){
-	include('user/forms/login');
+	if (isset($_GET['username']) && isset($_GET['password'])){
+		$url = getUrl('user/login?username='.$_GET['username'].'&password='.$_GET['password']);
+		$token = file_get_contents($url);
+		die($token);
+	}
+	include('user/form/login.php');
 	die();
 }
 die();
