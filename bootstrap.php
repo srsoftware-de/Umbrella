0be6c15bf4abde7a06ef4acfeb89b3d9213a7884
Stephan Richter <github@keawe.de>
2016-07-28 04:37:22 +0200

    implemented bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'config.php';
+
+function getUrl($service,$path){
+	global $services;
+	return $services[$service].$path;
+}
+
+function request($service,$path){
+	$url = getUrl($service,$path);
+	echo $url.'<br/>';
+	return file_get_contents($url);
+}
--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
 
-include 'config.php';
-
-function getUrl($service,$path){
-	global $services;
-	return $services[$service].$path;
-}
-
-function request($service,$path){
-	$url = getUrl($service,$path);
-	echo $url.'<br/>';
-	return file_get_contents($url);
-}
+include 'bootstrap.php';
 
 $token = null;
 if (isset($_GET['token'])) $token = $_GET['token'];
