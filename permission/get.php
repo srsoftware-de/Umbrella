e1978f33d58bc1eef1effc60e8cb5ed9c38b55a9
Stephan Richter <github@keawe.de>
2016-07-28 03:57:00 +0200

    adding scripts list to permissions

--- permission/get.php
+++ permission/get.php
@@ -1,17 +1,16 @@
 <?php
-function getUrl($path){
-	$proto = 'http';
-	if ($_SERVER['HTTPS'] == 'on') $proto.='s';
-	$proto.='://';
-	
-	$host = $_SERVER['HTTP_HOST'];
-	$script = dirname($_SERVER['SCRIPT_NAME']);
-	
-	return $proto.$host.$script.'/'.$path;  
+
+$scripts[]=array();
+$scripts['user']='https://eldorado.keawe.de:816/microservices/user/';
+
+function getUrl($script,$path){
+	global $scripts;
+		
+	return $scripts[$script].$path;  
 }
 
-function request($path){
-	$url = getUrl($path);
+function request($script,$path){
+	$url = getUrl($script,$path);
 	echo $url.'<br/>';
 	return file_get_contents($url);
 }
@@ -20,6 +19,6 @@ if (!isset($_GET['token'])) die(NULL);
 $token = $_GET['token'];
 if ($token == null) die(NULL);
 
-die(request('user/validate?token='.$token));
+die(request('user','validate?token='.$token));
 
 ?>
