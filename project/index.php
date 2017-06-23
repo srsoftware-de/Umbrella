0c60a146db479b6f51a159ef1e0fc5fd3be75bcc
Stephan Richter <github@keawe.de>
2017-06-23 17:14:11 +0200

    implemented optional display of closed projects n project index page

--- project/index.php
+++ project/index.php
@@ -5,18 +5,23 @@ include 'controller.php';
 
 require_login();
 $projects = get_project_list(param('order'));
-
+$show_closed = param('closed') == 'show';
 include '../common_templates/head.php'; 
 include '../common_templates/main_menu.php';
 include 'menu.php';
 include '../common_templates/messages.php'; ?>
 
+<?php if (!$show_closed){ ?>
+<a href="?closed=show">show closed projects</a>
+<?php }?>
 <table>
 	<tr>
 		<th><a href="?order=name">Name</a></th>
 		<th><a href="?order=status">Status</a></th>
 	</tr>
-<?php foreach ($projects as $id => $project): ?>
+<?php foreach ($projects as $id => $project){
+	if (!$show_closed && $project['status']>50) continue; 
+	?>
 	<tr>
 		<td><a href="<?= $id ?>/view"><?= $project['name'] ?></a></td>
 		<td><?= $PROJECT_STATES[$project['status']] ?></td>
@@ -27,7 +32,7 @@ include '../common_templates/messages.php'; ?>
 			<a href="<?= $id ?>/cancel">Cancel</a>
 		</td>
 	</tr>
-<?php endforeach; ?>
+<?php } ?>
 
 </table>
 <?php 
