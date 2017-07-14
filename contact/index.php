07436eb93bfaaa30bd45b8328c9bb8a68b232ad7
Stephan Richter <github@keawe.de>
2017-07-14 10:07:09 +0200

    improved contact menus

--- contact/index.php
+++ contact/index.php
@@ -12,12 +12,11 @@ include '../common_templates/messages.php'; ?>
 <div class="contacts">
 <?php foreach ($contacts as $id => $contact){ ?>
 	<fieldset>
-		<legend>Contact <?= $id ?></legend>
+		<legend><?= implode(', ',array_filter(explode(';', $contact['N']))) ?></legend>
 		<span>
-			<a href="<?= $id?>/download">Download</a>
-			<a href="<?= getUrl('files','add_user_to?file='.$file_hash) ?>">Share</a>
-			<a href="<?= $id?>/edit">Edit</a>
-			<a href="<?= $id?>/assign_with_me">Assign with me</a>
+			<a class="symbol" title="download" href="<?= $id?>/download"></a>
+			<a class="symbol" title="edit" href="<?= $id?>/edit"></a>
+			<a class="symbol" title="assign with me" href="<?= $id?>/assign_with_me"></a>
 		</span>
 		<?php debug ($contact)?>
 	</fieldset>
--- contact/menu.php
+++ contact/menu.php
@@ -1,4 +1,9 @@
 <div>
-<a href="index">Index</a>
-<a href="add">Add</a>
+	<?php if ($pid = param('id')){ ?>
+	<a class="symbol" href="../index"></a>
+	<a class="symbol" href="../add"></a>
+	<?php } else { ?>
+	<a class="symbol" href="index"></a>
+	<a class="symbol" href="add"></a>
+	<?php } ?>
 </div>
