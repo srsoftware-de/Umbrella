fa7421f680a472de30327096e8876a3144b96268
Stephan Richter <github@keawe.de>
2017-10-30 01:03:04 +0100

    Löschen von Tags implementiert

---
+++ tag/delete.php
@@ -0,0 +1,28 @@
+<?php $title = 'Umbrella Task Management';
+
+include '../bootstrap.php';
+include 'controller.php';
+
+require_login('tag');
+$url_hash = param('id');
+if (!$url_hash) error('No url hash passed to view!');
+
+$link = load_url($url_hash);
+if (isset($_POST['confirm']) && $_POST['confirm']==true) {
+	delete_link($link);
+	redirect('..');
+}
+
+include '../common_templates/head.php';
+include '../common_templates/main_menu.php';
+include 'menu.php';
+include '../common_templates/messages.php'; ?>
+
+<form method="POST">
+<fieldset>
+	<legend><?= t('Confirmation required') ?></legend>
+	<?= t('Are you sure you want to delete "?" (?) ?',[$link['comment'],$link['url']]); ?><br/>
+	<button type="submit" name="confirm" value="false">No</button><button type="submit" name="confirm" value="true">Yes</button>
+</fieldset>
+</form>
+<?php include '../common_templates/closure.php'; ?>
