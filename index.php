ada1094b421bd58dc1bbef31d4b5adb9144f494f
Stephan Richter <github@keawe.de>
2017-11-08 11:05:24 +0100

    added jump menu at bookmark index page

--- index.php
+++ index.php
@@ -10,10 +10,26 @@ include '../common_templates/main_menu.php';
 include 'menu.php';
 include '../common_templates/messages.php'; ?>
 
+<div class="hover right-fix">
+	<?php
+		$break =false; 
+		foreach (['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'] as $letter) { ?>
+	<a class="button" href="#<?= $letter ?>">&nbsp;&nbsp;<?= $letter; ?>&nbsp;&nbsp;</a>
+	<?= $break ? '<br/>':''?>
+	<?php $break = !$break; }?>
+</div>
 <fieldset>
 	<legend>Tags</legend>
-<?php foreach ($tags as $tag => $data){ ?>
-	<a class="button" href="<?= urlencode($tag).'/view' ?>"><?= $tag ?></a>
+<?php
+	$letter = null;
+	foreach ($tags as $tag => $data){
+		$id = false;
+		$char = strtoupper($tag[0]);
+		if ($letter != $char){
+			$letter = $char;
+			$id = true;
+		} // if ?>
+	<a <?= $id ?'id="'.$letter.'" ':' ' ?>class="button" href="<?= urlencode($tag).'/view' ?>"><?= $tag ?></a>
 <?php } ?>
 </fieldset>
 
