af9d34158e0257a7f1c5e555cd746a725eee58cc
Stephan Richter <github@keawe.de>
2017-11-09 22:28:11 +0100

    fixed typo in config template

--- config.template.php
+++ config.template.php
@@ -3,7 +3,7 @@ $services=array();
 $services['project']=array('name'=>'Projects','path'=>'https://umbrella.example.com/project/');
 $services['task']	=array('name'=>'Tasks','path'=>'https://umbrella.example.com/task/');
 $services['time']	=array('name'=>'Timetrack','path'=>'https://umbrella.example.com/time/');
-$services['incoive']=array('name'=>'Invoices','path'=>'https://umbrella.example.com/invoice/');
+$services['invoice']=array('name'=>'Invoices','path'=>'https://umbrella.example.com/invoice/');
 $services['user']	=array('name'=>'Users','path'=>'https://umbrella.example.com/user/');
 $services['contact']=array('name'=>'Contacts','path'=>'https://umbrella.example.com/contact/');
 $services['bookmark']=array('name'=>'Bookmarks','path'=>'https://umbrella.example.com/bookmark/');
