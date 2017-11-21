a1b1afe6dafb86ec2da2e30b013341873020fa8b
Stephan Richter <github@keawe.de>
2017-11-22 00:54:35 +0100

    changed menu symbol

--- menu.php
+++ menu.php
@@ -1,3 +1,3 @@
 <a class="symbol" title="<?= t('add link') ?>" href="<?= $base_url?>add"></a>
-<a class="symbol" title="<?= t('latest bookmarks')?>" href="<?= $base_url?>tags"></a>
+<a class="symbol" title="<?= t('latest bookmarks')?>" href="<?= $base_url?>tags"></a>
 <a class="symbol" title="<?= t('import from delicious')?>" href="<?= $base_url?>import"></a>
