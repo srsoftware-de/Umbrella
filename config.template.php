44f676ea7fc6620d09cffdbce61e46e00213e18c
Stephan Richter <github@keawe.de>
2017-12-08 17:16:34 +0100

    added new branch item

--- config.template.php
+++ config.template.php
@@ -1,14 +1,16 @@
 <?php
 // alter order to restructure the main menu order
 $services=array();
-$services['project']=array('name'=>'Projects','path'=>'https://umbrella.example.com/project/');
-$services['task']	=array('name'=>'Tasks','path'=>'https://umbrella.example.com/task/');
-$services['time']	=array('name'=>'Timetrack','path'=>'https://umbrella.example.com/time/');
-$services['invoice']=array('name'=>'Invoices','path'=>'https://umbrella.example.com/invoice/');
-$services['user']	=array('name'=>'Users','path'=>'https://umbrella.example.com/user/');
-$services['contact']=array('name'=>'Contacts','path'=>'https://umbrella.example.com/contact/');
-$services['bookmark']=array('name'=>'Bookmarks','path'=>'https://umbrella.example.com/bookmark/');
-$services['files']	=array('name'=>'Files','path'=>'https://umbrella.example.com/files/');
-$services['company']	=array('name'=>'Company','path'=>'https://umbrella.example.com/company/');
+$services['project']	=array('name'=>'Projects',	'path'=>'https://umbrella.keawe.de/project/');
+$services['time']		=array('name'=>'Timetrack',	'path'=>'https://umbrella.keawe.de/time/');
+$services['task']		=array('name'=>'Tasks',		'path'=>'https://umbrella.keawe.de/task/');
+$services['user']		=array('name'=>'Users',		'path'=>'https://umbrella.keawe.de/user/');
+$services['invoice']	=array('name'=>'Invoices',	'path'=>'https://umbrella.keawe.de/invoice/');
+$services['items']  	=array('name'=>'Items',		'path'=>'https://umbrella.keawe.de/item/');
+$services['contact']	=array('name'=>'Contacts',	'path'=>'https://umbrella.keawe.de/contact/');
+$services['bookmark']	=array('name'=>'Bookmarks',	'path'=>'https://umbrella.keawe.de/bookmark/');
+$services['files']		=array('name'=>'Files',		'path'=>'https://umbrella.keawe.de/files/');
+$services['company']	=array('name'=>'Company',	'path'=>'https://umbrella.keawe.de/company/');
+
 $lang = 'de';
 ?>
--- time/json_list.php
+++ time/json_list.php
@@ -8,7 +8,7 @@ include 'controller.php';
 	$times = get_time_list();
 	$assignments = get_time_assignments(array_keys($times));
 	foreach($times as $time_id => &$time){
-		$time['tasks'] = $assignments[$time_id];
+		if (isset($assignments[$time_id])) $time['tasks'] = $assignments[$time_id];
 	}	
 	echo json_encode($times);
 ?>
