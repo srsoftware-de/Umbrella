a5d9500f24d210820aeee85fdad3a8e2368e43c1
Stephan Richter <github@keawe.de>
2018-01-01 21:42:47 +0100

    altered select_dir.php according to previous changes

--- select_dir.php
+++ select_dir.php
@@ -4,42 +4,40 @@ include '../bootstrap.php';
 include 'controller.php';
 
 require_login('files');
-$path = param('path','user'.$user->id);
 $target = param('target');
+
 if ($target === null) redirect('index');
+$path = param('path');
+if (in_array($path,['.','user'])) $path = null;
 $entries = list_entries($path);
 $parent = dirname($path);
+
+$message=param('message',t('Select a folder:'));
 include '../common_templates/head.php'; 
 include '../common_templates/main_menu.php';
 include 'menu.php';
 include '../common_templates/messages.php'; ?>
 
-<h1><?= t('Files: ?',$path?$path:' ')?></h1>
+<h2><?= $message ?></h2>
+<?php if ($path) { ?><h3><?= t('current folder: ?',$path) ?></h3><?php } ?>
+
 <table>
 	<tr>
 		<th><?= t('File / Directory') ?></th>
 		<th><?= t('Action') ?></th>
 	</tr>
-	<?php if (!in_array($parent,['.',''])){ ?>
-	<tr>
-		<td>
-			<a title="<?= t('move one directory up') ?>" href="?target=<?= $target?>&path=<?= $parent ?>">
-				<span class="symbol"></span> ..
-			</a>
-		</td>
-		<td></td>
-	</tr>
-	<?php } ?>
-	<?php foreach ($entries['dirs'] as $dir){ 
+	<?php foreach ($entries['dirs'] as $alias => $dir){ 
 	?>
 	<tr>
 		<td>
-			<a href="?target=<?= $target?>&path=<?= $path.DS.$dir ?>">
-				<span class="symbol"></span> <?= $dir ?>
+			<a href="?target=<?= $target.'&message='.urlencode($message) ?>&path=<?= $dir ?>">
+				<span class="symbol"></span> <?= $alias ?>
 			</a>
 		</td>
 		<td>
-			<a href="<?= $target ?>?path=<?= urlencode($path.DS.$dir)?>"><?= t('select') ?></a>
+			<?php if ($alias != '..') { ?>
+			<a href="<?= $target ?>?path=<?= urlencode($dir)?>"><?= t('select') ?></a>
+			<?php }?>
 		</td>
 		
 	</tr>
