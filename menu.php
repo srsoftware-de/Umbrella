6b760111cc3faf5209a4b766e9da9b82cf965d4c
Stephan Richter <github@keawe.de>
2018-01-03 16:20:43 +0100

    bugfix

--- menu.php
+++ menu.php
@@ -1,3 +1,3 @@
-<a class="symbol" title="<?= t('add link') ?>" href="<?= $base_url?>add"></a>
-<a class="symbol" title="<?= t('all tags')?>" href="<?= $base_url?>tags"></a>
-<a class="symbol" title="<?= t('import from delicious')?>" href="<?= $base_url?>import"></a>
+<a class="symbol" title="<?= t('add link') ?>" href="<?= getUrl('bookmark','add')?>"></a>
+<a class="symbol" title="<?= t('all tags')?>" href="<?= getUrl('bookmark','tags') ?>"></a>
+<a class="symbol" title="<?= t('import from delicious')?>" href="<?= getUrl('bookmark','import')?>"></a>
