cb6e5b1bab4df14c360fb762a673caaae0a17f20
Stephan Richter <github@keawe.de>
2018-01-04 11:27:48 +0100

    restricted addable users to non-participants

--- add_user.php
+++ add_user.php
@@ -11,23 +11,25 @@ if (!$task_id) error('No task id passed to view!');
 
 $task = load_tasks(['ids'=>$task_id]);
 
-$project_users_ids = request('project','user_list',['id'=>$task['project_id']]);
-$project_users = request('user','list',['ids'=>implode(',', array_keys($project_users_ids))]);
-
+// get a map from user ids to permissions
+$project_user_permissions = request('project','user_list',['id'=>$task['project_id']]);
+$project_user_ids = array_keys($project_user_permissions);
+$project_users = request('user','list',['ids'=>implode(',', $project_user_ids)]);
 load_users($task,$project_users);
+
+foreach ($task['users'] as $uid => $task_user) unset($project_users[$uid]);
 
 $title = $task['name'].' - Umbrella';
 
-$allowed = false;
-foreach ($task['users'] as $id => $u){
-	if ($id == $user->id){
-		if ($u['permissions'] & TASK_PERMISSION_OWNER) {
-			$allowed = true;
-		} elseif ($u['permissions'] & TASK_PERMISSION_PARTICIPANT) $allowed = true;
-	}
-}
+$allowed = isset($task['users'][$user->id]['permissions'])
+           && in_array($task['users'][$user->id]['permissions'], [TASK_PERMISSION_OWNER,TASK_PERMISSION_PARTICIPANT]);
 
 if ($allowed){
+	if (empty($project_users)) {
+		warn('All members of this project are already assigned to this task.');
+		redirect('view');
+	}
+	
 	if ($task_user = post('task_user')){
 		add_user_to_task($task_id,$task_user,post('permissions'));
 		redirect('view');
@@ -45,7 +47,8 @@ if ($allowed){ ?>
 		<fieldset>
 			<select name="task_user">
 				<option value="" selected="true">= Select a user =</option>
-				<?php foreach ($project_users as $id => $u){ ?>
+				<?php foreach ($project_users as $id => $u){
+					if (in_array($id, $project_users_ids)) continue; ?>
 				<option value="<?= $id ?>"><?= $u['login']?></option>
 				<?php }?>
 			</select>
