60b46092c3f03415d5382cf1d07cc282d2e59bd8
Stephan Richter <github@keawe.de>
2018-01-04 15:26:46 +0100

    bugfix

--- user/list.php
+++ user/list.php
@@ -5,5 +5,5 @@ include 'controller.php';
 
 require_user_login();
 $ids = param('ids');
-if ($ids) $ids = explode(',',$ids);
+if ($ids && !is_array($ids)) $ids = explode(',',$ids);
 echo json_encode(get_userlist($ids));
