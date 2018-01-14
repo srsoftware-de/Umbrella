dd2d5dab9341a79a3ab297e1435ee13390f86199
Stephan Richter <github@keawe.de>
2018-01-14 14:41:30 +0100

    bugfix

--- json.php
+++ json.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 include '../bootstrap.php';
 include 'controller.php';
@@ -7,7 +7,7 @@ require_login('time');
 
 $options = [];
 if ($ids = param('ids'))$options['ids'] = explode(',',$ids);
-if ($task_ids = param('task_ids')) $options['task_ids'] = explode(',',$task_ids);
+if ($task_ids = param('task_ids')) $options['task_ids'] = $task_ids;
 if ($ids_only = param('ids_only')) $options['ids_only'] = $ids_only;
 
 die(json_encode(load_times($options)));
