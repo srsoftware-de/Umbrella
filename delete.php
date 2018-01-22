4fc8ab45bbe101cf56412812e464f5de4c86195f
Stephan Richter <github@keawe.de>
2018-01-22 09:59:07 +0100

    improved template

--- delete.php
+++ delete.php
@@ -4,7 +4,7 @@ include '../bootstrap.php';
 include 'controller.php';
 require_login('task');
 
-if ($task_id = param('id');
+$task_id = param('id');
 if (!$task_id) {
 	error('No task id passed!');
 	redirect(getUrl('task'));
@@ -31,10 +31,11 @@ include '../common_templates/main_menu.php';
 include '../common_templates/messages.php'; 
 
 if ($task){ ?>
-<h2><?= t('This will remove the task "?". Are you sure?',$task['name'])?></h2>
-<a href="?confirm=yes<?= $target?('&redirect='.$target):''?>" class="button"><?= t('Yes')?></a>
-<a href="view" class="button"><?= t('No')?></a>
-
+<fieldset>
+	<legend><?= t('This will remove the task "?". Are you sure?',$task['name'])?></legend>
+	<a href="?confirm=yes<?= $target?('&redirect='.$target):''?>" class="button"><?= t('Yes')?></a>
+	<a href="view" class="button"><?= t('No')?></a>
+</fieldset>
 <?php }
 
 include '../common_templates/closure.php';?>
