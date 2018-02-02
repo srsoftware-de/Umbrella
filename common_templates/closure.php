71983998905a3ff3319e6640008369b42fa6906f
Stephan Richter <github@keawe.de>
2018-02-02 01:36:39 +0100

    created separate branch for user dir

--- common_templates/closure.php
+++
@@ -1,4 +0,0 @@
-<br/>
-<span id="announce"><a target="_blank" href="https://github.com/keawe-software/Umbrella" ><?= t('Umbrella : open-source project management')?></a></span>
-</body>
-</html>
--- common_templates/main_menu.php
+++
@@ -1,8 +0,0 @@
-<div id="main_menu">
-<?php foreach ($services as $service){ ?>
-	<a class="button" href="<?= $service['path'] ?>"><?= t($service['name']) ?></a>
-<?php } 
-if (isset($user)) { ?>
-	<a class="button" href="<?= $services['user']['path'].'logout?returnTo='.location() ?>"><?= t('Log out ?',$user->login) ?></a>
-<?php } ?>
-</div>
