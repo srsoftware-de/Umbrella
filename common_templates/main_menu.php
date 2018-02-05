85203d93a4a776af7eb8bd594bff515c20975dfc
Stephan Richter <github@keawe.de>
2018-02-05 16:12:55 +0100

    altered default template for static pages

--- common_templates/main_menu.php
+++ common_templates/main_menu.php
@@ -1,8 +1,8 @@
 <div id="main_menu">
-<?php foreach ($services as $service){ ?>
+<?php foreach ($services as $service){ if ($service['name'] == 'Invoices') continue;?>	
 	<a class="button" href="<?= $service['path'] ?>"><?= t($service['name']) ?></a>
 <?php } 
 if (isset($user)) { ?>
 	<a class="button" href="<?= $services['user']['path'].'logout?returnTo='.location() ?>"><?= t('Log out ?',$user->login) ?></a>
-<?php } ?>
+<?php }?>
 </div>
