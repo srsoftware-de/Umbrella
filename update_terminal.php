7001cff4cce73fcc9b198fe90e48ae513a37bb60
Stephan Richter <github@keawe.de>
2018-04-15 12:04:03 +0200

    working on terminal interaction

---
+++ update_terminal.php
@@ -0,0 +1,23 @@
+<?php $title = 'Umbrella Model Management';
+
+include '../bootstrap.php';
+include 'controller.php';
+
+require_login('model');
+
+$model_id = param('id1');
+$terminal_id = param('id2');
+
+if (!$model_id){
+	error('No model id passed to terminal.');
+	redirect(getUrl('model'));
+}
+if (!$terminal_id){
+	error('No terminal id passed to terminal.');
+	redirect(getUrl('model'));
+}
+
+$model = Model::load(['ids'=>$model_id]);
+$terminal = $model->terminals($terminal_id);
+$terminal->patch($_POST);
+$terminal->save();
