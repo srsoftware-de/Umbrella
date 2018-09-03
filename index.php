963bd9b4ca20c8a5331a587c12fbeaf58dc9951d
Stephan Richter <github@keawe.de>
2018-09-04 00:05:00 +0200

    working on rtc implementation

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 require_login('rtc');
 
 $channels = Channel::load();
-$base_url = getUrl('rtc','open/');
+$base_url = getUrl('rtc');
+
 include '../common_templates/head.php';
 include '../common_templates/main_menu.php';
 include 'menu.php'; 
@@ -13,14 +14,18 @@ include '../common_templates/messages.php'; ?>
 	<tr>
 		<th><?= t('Channel') ?></th>
 		<th><?= t('Users') ?></th>
+		<th><?= t('Actions') ?></th>
 	</tr>
 	<?php foreach ($channels as $id => $channel){ ?>
 	<tr>
-		<td><a href="<?= $base_url.$id ?>"><?= $channel->subject ?></a></td>
+		<td><a target="_blank" href="<?= $base_url.$id.'/open' ?>"><?= $channel->subject ?></a></td>
 		<td>
 		<?php foreach ($channel->users() as $u){ ?> 
 			<span><?= $u['login']?></span>	
 		<?php }?>
+		</td>
+		<td>
+			<a class="symbol" title="<?= t('add user') ?>" href="<?= $id ?>/add_user"> </a>
 		</td>
 	</tr>
 	<?php } ?>
--- lang.de.php
+++ lang.de.php
@@ -1,7 +1,9 @@
 <?php
 
 $translations = array(
+		'Actions'=>'Aktionen',
 		'Bookmarks' => 'Lesezeichen',
+		'Channel'=>'Kanal',
 		'Company'=>'Firma',
 		'Contacts'=>'Kontakte',
 		'Documents'=>'Dokumente',
@@ -10,7 +12,9 @@ $translations = array(
 		'Items'=>'Artikel',
 		'Log out ?'=>'? ausloggen',
 		'Most functions will not work at the moment.'=>'Die meisten Funktionen funktionieren im Moment nicht.',
+		'new conversation'=>'neue Unterhaltung',
 		'Notes'=>'Notizen',
+		'Subject'=>'Thema',
 		'Tasks'=>'Aufgaben',
 		'The RTC module is currently under development.'=>'Das RTC-Modul ist im Moment noch in der Entwicklung.',
 		'Timetrack'=>'Zeiterfassung',
