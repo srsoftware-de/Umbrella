47ec2932bc4288e080e1801d266c47eca2a1a204
Stephan Richter <github@keawe.de>
2018-09-04 14:40:33 +0200

    bugfixes

--- open.php
+++ open.php
@@ -7,7 +7,8 @@ if ($hash = param('id')) $channel = Channel::load(['hashes'=>$hash]);
 if ($users = param('users')) {
 	if (!is_array($users)) $users = explode(',',$users); 
 	$channel = Channel::load(['users'=>$users]);
-	if (empty($channel)) $channel = (new Channel())->patch(['users'=>$users]); // create new channel for users
+
+	if (empty($channel)) $channel = (new Channel())->patch(['users'=>$users])->save(); // create new channel for users
 }
 if ($channel) $channel->open();
 redirect(getUrl('rtc'));
