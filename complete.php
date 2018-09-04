88262a604435f301b4d5fbc011177fb84dda1ae7
Stephan Richter <github@keawe.de>
2018-09-04 22:55:12 +0200

    bugfix

--- complete.php
+++ complete.php
@@ -10,6 +10,6 @@ if ($target = param('redirect')) {
 	redirect($target);
 } else {
 	$task = load_tasks(['ids'=>$task_id]);
-	if (isset($task['parent_task_id']) && $task['parent_task_id']!==null) redirect(getUrl('task',$task['parent_task_id'].'/view'));
+	if (!empty($task['parent_task_id']) && $task['parent_task_id']!==null) redirect(getUrl('task',$task['parent_task_id'].'/view'));
 	redirect(getUrl('project',$task['project_id'].'/view'));
 }
