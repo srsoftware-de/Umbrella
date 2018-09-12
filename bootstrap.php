49f7e85656bcb0da084d76d2179d92811794254a
Stephan Richter <github@keawe.de>
2018-09-12 08:33:47 +0200

    re-ordered methods alphabetically

--- bootstrap.php
+++ bootstrap.php
@@ -19,6 +19,168 @@ const TASK_STATUS_PENDING = 40;
 const TASK_STATUS_COMPLETE = 60;
 const TASK_STATUS_CANCELED = 100;
 
+function address_from_vcard($vcard){
+	$result = '';
+	if (isset($vcard->FN)) $result .= $vcard->FN."\n";
+	if (isset($vcard->N)) $result .= trim($vcard->N->given.' '.$vcard->N->family) . "\n";
+	$address = $vcard->ADR;
+	if (is_array($address)) $address = reset($address);
+	if (!isset($address->post_box)) $result .= t('Postbox: ?',$address->post_box)."\n"; // Postfach
+	if (!empty($address->ext_addr)) $result .= $address->ext_addr."\n";				// Adresszusatz
+	if (!empty($address->street)) $result .=	$address->street."\n";				// Straße
+	if (!empty($address->post_code)) $result .=	$address->post_code." ";				// Postleitzahl
+	if (!empty($address->locality)) $result .=	$address->locality."\n";				// Ort
+	if (!empty($address->region)) $result .=	$address->region." / ";				// Region
+	if (!empty($address->country)) $result .=	$address->country;				// Land
+
+	return $result;
+}
+
+function assert_failure($script, $line, $code, $message){
+	error('Assertion failed in '.$script.', line '.$line.': '.$message);
+	include 'common_templates/messages.php';
+	include 'common_templates/closure.php';
+	die();
+}
+
+function conclude_vcard($vcard){
+	$result = '';
+	if (isset($vcard->FN)) $result = trim($vcard->FN);
+	if ($result != '') return $result;
+	if (isset($vcard->N)) $result = trim($vcard->N->given.' '.$vcard->N->family);
+	if ($result != '') return $result;
+	debug('error in conclude_vcard: no name set',true);
+}
+
+function debug($object,$die = false,$exclude = false){
+	if ($object === null) {
+		echo 'null';
+	} else if ($object === false) {
+		echo 'false';
+	} else {
+		if ($exclude){
+			if (!is_array($exclude)) $exclude = [$exclude];
+			if (is_array($object)){
+				foreach ($exclude as $key) unset($object[$key]);
+			} else foreach ($exclude as $key) unset($object->{$key});
+		}
+		echo '<pre>'.print_r($object,true).'</pre>';
+	}
+	if ($die){
+		include 'common_templates/closure.php';
+		die();
+	}
+}
+
+function dialog($question,$options = array('YES'=>'?confirm=yes','NO'=>'index')){
+	$result = '<fieldset class="dialog">'.$question.'</br>';
+	foreach ($options as $text => $link){
+		$result .= '<a class="button" href="'.$link.'">'.$text.'</a>&nbsp';
+	}
+	return $result.'</fieldset>';
+}
+
+function error($message,$args = null){
+	if ($message === null) return;
+	$_SESSION['errors'][crc32($message)] = t($message,$args);
+}
+
+function getLocallyFromToken(){
+	$db = get_or_create_db();
+	$db->query('CREATE TABLE IF NOT EXISTS tokens (token VARCHAR(255) NOT NULL PRIMARY KEY, expiration INT NOT NULL, user_data TEXT NOT NULL);');
+
+	$query = $db->prepare('SELECT * FROM tokens WHERE token = :token;');
+	$params = array(':token' => $_SESSION['token']);
+	assert($query->execute($params),'Was not able to request token table.');
+	$rows = $query->fetchAll(PDO::FETCH_ASSOC);
+	$time = time();
+	$user = null;
+
+	$query = $db->prepare('DELETE FROM tokens WHERE token = :token;');
+	foreach ($rows as $index => $row){
+		if ($row['expiration'] > $time){
+			$user = json_decode($row['user_data']); // read user data
+		} else 	$query->execute([':token'=>$row['token']]); // drop expired token
+	}
+	return $user;
+}
+
+function getUrl($service_name,$path=''){
+	global $services;
+	assert($service_name !== null,'No service handed to getUrl!');
+	assert(isset($services[$service_name]['path']),'No '.$service_name.' service configured!');
+	$url = $services[$service_name]['path'].$path;
+	return $url;
+}
+
+function html2plain($text){
+	$text = str_replace(['<br/>','<br />','<br>'],"\n",$text);
+	$text = str_replace(['<li>'],"- ",$text);
+	$text = str_replace(['</li>','<ul>','</ul>'],'',$text);
+	$text = str_replace('"','""',$text);
+	return $text;
+}
+
+function info($message,$args = null){
+	if ($message === null) return;
+	$_SESSION['infos'][crc32($message)] = t($message,$args);
+}
+
+function init(){
+	global $user;
+	$user = null;
+	session_start();
+	if ($token_param = param('token')) $_SESSION['token'] = $token_param;
+	if (isset($_GET['token'])) redirect(location('token')); // if token was appended to url: set cookie and reload
+}
+
+function location($drop = []){
+	if ($drop == '*'){
+		$args = [];
+	} else {
+		if (!is_array($drop)) $drop = [$drop];
+		$args = $_GET;
+		foreach ($drop as $key) unset($args[$key]);
+	}
+	$port = $_SERVER['SERVER_PORT'];
+	$get_string = empty($args)?'':'?'.http_build_query($args);
+	return $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].($port == 80 || $port == 443?'':':'.$port).$_SERVER['REDIRECT_URL'].$get_string;
+}
+
+function module_version(){
+	if (!defined('MODULE')) return '';
+	if (empty($_SESSION['modules'])) $_SESSION['modules'] = [];
+	if (!isset($_SESSION['modules'][MODULE])) $_SESSION['modules'][MODULE] = exec('git show -s --format=%ci HEAD');
+	return '"'.MODULE.'" module ('.$_SESSION['modules'][MODULE].') - ';
+}
+
+function objectFrom($entity){
+	if (is_array($entity)){
+		$obj = new stdClass();
+		foreach ($entity as $key => $val) $obj->$key = objectFrom($val);
+		return $obj;
+	}
+	return $entity;
+}
+
+function param($name,$default = null){
+	if (isset($_GET[$name])) {
+		$result = $_GET[$name];
+		if (is_array($result) || is_object($result)) return $result;
+		return trim($result);
+	}
+	return post($name,$default);
+}
+
+function post($name,$default = null){
+	if (isset($_POST[$name])){
+		$result = $_POST[$name];
+		if (is_array($result) || is_object($result)) return $result;
+		return trim($result);
+	}
+	return $default;
+}
+
 function project_state($state){
 	switch ($state){
 		case PROJECT_STATUS_CANCELED: return 'canceled';
@@ -30,30 +192,34 @@ function project_state($state){
 	return 'unknown';
 }
 
-function task_state($state){
-	switch ($state){
-		case TASK_STATUS_OPEN: return 'open';
-		case TASK_STATUS_PENDING: return 'pending';
-		case TASK_STATUS_STARTED: return 'started';
-		case TASK_STATUS_CANCELED: return 'canceled';
-		case TASK_STATUS_COMPLETE : return 'completed';		
+function query_insert($query,$args){
+	$sql = ($query instanceof PDOStatement ? $query->queryString : $query) . ' ';
+	$pos = strpos($sql,'?');
+	if ($pos > 0){
+		while ($pos > 0){
+			$sql = substr_replace($sql,array_shift($args),$pos,1);
+			$pos = strpos($sql,'?');
+		}
+	} else {
+		foreach ($args as $k => $v){
+			$sql = str_replace($k.',','"'.$v.'",',$sql);
+			$sql = str_replace($k.' ','"'.$v.'" ',$sql);
+		}
 	}
-	return 'unknown';
+	return $sql;
 }
 
-function assert_failure($script, $line, $code, $message){
-	error('Assertion failed in '.$script.', line '.$line.': '.$message);
-	include 'common_templates/messages.php';
-	include 'common_templates/closure.php';
+function redirect($url){
+	header('Location: '.$url);
 	die();
 }
 
-function getUrl($service_name,$path=''){
-	global $services;
-	assert($service_name !== null,'No service handed to getUrl!');
-	assert(isset($services[$service_name]['path']),'No '.$service_name.' service configured!');
-	$url = $services[$service_name]['path'].$path;
-	return $url;
+function replace_text($text,$replacements = null){
+	if ($replacements !== null){
+		if (!is_array($replacements)) $replacements = array($replacements);
+		while ($rep = array_shift($replacements)) $text = preg_replace('/\?/', $rep, $text,1);
+	}
+	return $text;
 }
 
 function request($service = null,$path,$data = array(), $debug = false,$decode = ARRAY_CONVERSION){
@@ -95,6 +261,27 @@ function request($service = null,$path,$data = array(), $debug = false,$decode =
 	return $response;
 }
 
+/**
+ * checks if a user is logged in and forces a login of not.
+ */
+function require_login($service_name = null){
+	global $services,$user,$theme;
+	if ($revoke = param('revoke')) die(revoke_token($revoke));
+	assert($service_name !== null,'require_login called without a service name!');
+	if (!isset($_SESSION['token']) || $_SESSION['token'] === null) redirect(getUrl('user','login?returnTo='.urlencode(location())));
+	$user = getLocallyFromToken();
+	if ($user === null) validateToken($service_name);
+	if ($user === null) redirect(getUrl('user','login?returnTo='.urlencode(location())));
+	if (isset($user->theme)) $theme = $user->theme;
+}
+
+function revoke_token($token){
+	$db = get_or_create_db();
+	$query = $db->prepare('DELETE FROM tokens WHERE token = :token');
+	assert($query->execute(array(':token'=>$token)),'Was not able to execute DELETE statement.');
+	unset($_SESSION['token']);
+}
+
 function save_file($filename,$file_contents,$mime){
 	$url = getUrl('files','add');
 
@@ -102,14 +289,14 @@ function save_file($filename,$file_contents,$mime){
 	$br = "\r\n";
 
 	$content =  '--'.$boundary.$br.
-				'Content-Disposition: form-data; name="file"; filename="'.basename($filename).'"'.$br.
-				'Content-Type: '.$mime.$br.$br.$file_contents.$br;
+	'Content-Disposition: form-data; name="file"; filename="'.basename($filename).'"'.$br.
+	'Content-Type: '.$mime.$br.$br.$file_contents.$br;
 
 	$content .=	'--'.$boundary.$br.
-				'Content-Disposition: form-data; name="token"'.$br.$br.$_SESSION['token'].$br;
+	'Content-Disposition: form-data; name="token"'.$br.$br.$_SESSION['token'].$br;
 
 	$content .=	'--'.$boundary.$br.
-				'Content-Disposition: form-data; name="dir"'.$br.$br.dirname($filename).$br;
+	'Content-Disposition: form-data; name="dir"'.$br.$br.dirname($filename).$br;
 
 
 	$content .= "--".$boundary.'--'.$br;
@@ -118,15 +305,15 @@ function save_file($filename,$file_contents,$mime){
 
 	$http_options = array();
 	$http_options['method'] = 'POST';
-	$http_options['header'] = 'Content-Type: multipart/form-data; boundary='.$boundary;	
+	$http_options['header'] = 'Content-Type: multipart/form-data; boundary='.$boundary;
 	$http_options['content'] = $content;
 
 	$options = [
-			'ssl' => $ssl_options,
-			'http'=> $http_options,
+	'ssl' => $ssl_options,
+	'http'=> $http_options,
 	];
 
-	$context = stream_context_create($options);	
+	$context = stream_context_create($options);
 
 	$response = file_get_contents($url,false,$context);
 }
@@ -167,121 +354,25 @@ function send_mail($sender, $reciever, $subject, $text, $attachment = null){
 	return $good;
 }
 
-function post($name,$default = null){
-	if (isset($_POST[$name])){
-		$result = $_POST[$name];
-		if (is_array($result) || is_object($result)) return $result;
-		return trim($result);
+function t($text,$replacements=null){
+	global $lang;
+	$lang_file ='lang.'.$lang.'.php';
+	if (file_exists($lang_file)){
+		include $lang_file;
+		if (isset($translations[$text])) $text=$translations[$text];
 	}
-	return $default;
+	return replace_text($text,$replacements);
 }
 
-function param($name,$default = null){
-	if (isset($_GET[$name])) {
-		$result = $_GET[$name];
-		if (is_array($result) || is_object($result)) return $result;
-		return trim($result);
+function task_state($state){
+	switch ($state){
+		case TASK_STATUS_OPEN: return 'open';
+		case TASK_STATUS_PENDING: return 'pending';
+		case TASK_STATUS_STARTED: return 'started';
+		case TASK_STATUS_CANCELED: return 'canceled';
+		case TASK_STATUS_COMPLETE : return 'completed';		
 	}
-	return post($name,$default);
-}
-
-function info($message,$args = null){
-	if ($message === null) return;
-	$_SESSION['infos'][crc32($message)] = t($message,$args);
-}
-
-function warn($message,$args = null){
-	if ($message === null) return;
-	$_SESSION['warnings'][crc32($message)] = t($message,$args);
-}
-
-
-function error($message,$args = null){
-	if ($message === null) return;
-	$_SESSION['errors'][crc32($message)] = t($message,$args);
-}
-
-function debug($object,$die = false,$exclude = false){
-	if ($object === null) {
-		echo 'null';
-	} else if ($object === false) {
-		echo 'false';
-	} else {
-		if ($exclude){
-			if (!is_array($exclude)) $exclude = [$exclude];
-			if (is_array($object)){
-				foreach ($exclude as $key) unset($object[$key]);
-			} else foreach ($exclude as $key) unset($object->{$key});
-		}
-		echo '<pre>'.print_r($object,true).'</pre>';
-	}
-	if ($die){
-		include 'common_templates/closure.php';
-		die();
-	}
-}
-
-function query_insert($query,$args){
-	$sql = ($query instanceof PDOStatement ? $query->queryString : $query) . ' ';
-	$pos = strpos($sql,'?');
-	if ($pos > 0){
-		while ($pos > 0){
-			$sql = substr_replace($sql,array_shift($args),$pos,1);
-			$pos = strpos($sql,'?');
-		}
-	} else {
-		foreach ($args as $k => $v){
-			$sql = str_replace($k.',','"'.$v.'",',$sql);
-			$sql = str_replace($k.' ','"'.$v.'" ',$sql);
-		}
-	}
-	return $sql;
-}
-
-function redirect($url){
-	header('Location: '.$url);
-	die();
-}
-
-function replace_text($text,$replacements = null){
-	if ($replacements !== null){
-		if (!is_array($replacements)) $replacements = array($replacements);
-		while ($rep = array_shift($replacements)) $text = preg_replace('/\?/', $rep, $text,1);
-	}
-	return $text;
-}
-
-function location($drop = []){
-	if ($drop == '*'){
-		$args = [];
-	} else {
-		if (!is_array($drop)) $drop = [$drop];
-		$args = $_GET;
-		foreach ($drop as $key) unset($args[$key]);
-	}
-	$port = $_SERVER['SERVER_PORT'];
-	$get_string = empty($args)?'':'?'.http_build_query($args);
-	return $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].($port == 80 || $port == 443?'':':'.$port).$_SERVER['REDIRECT_URL'].$get_string;
-}
-
-function getLocallyFromToken(){
-	$db = get_or_create_db();
-	$db->query('CREATE TABLE IF NOT EXISTS tokens (token VARCHAR(255) NOT NULL PRIMARY KEY, expiration INT NOT NULL, user_data TEXT NOT NULL);');
-
-	$query = $db->prepare('SELECT * FROM tokens WHERE token = :token;');
-	$params = array(':token' => $_SESSION['token']);
-	assert($query->execute($params),'Was not able to request token table.');
-	$rows = $query->fetchAll(PDO::FETCH_ASSOC);
-	$time = time();
-	$user = null;
-
-	$query = $db->prepare('DELETE FROM tokens WHERE token = :token;');
-	foreach ($rows as $index => $row){
-		if ($row['expiration'] > $time){
-			$user = json_decode($row['user_data']); // read user data
-		} else 	$query->execute([':token'=>$row['token']]); // drop expired token
-	}
-	return $user;
+	return 'unknown';
 }
 
 /* uses the user service to validate the session token and get user data */
@@ -299,101 +390,9 @@ function validateToken($service_name = null){
 	} else $user = null;
 }
 
-function revoke_token($token){
-	$db = get_or_create_db();
-	$query = $db->prepare('DELETE FROM tokens WHERE token = :token');
-	assert($query->execute(array(':token'=>$token)),'Was not able to execute DELETE statement.');
-	unset($_SESSION['token']);	
-}
-
-/**
- * checks if a user is logged in and forces a login of not.
- */
-function require_login($service_name = null){
-	global $services,$user,$theme;
-	if ($revoke = param('revoke')) die(revoke_token($revoke));
-	assert($service_name !== null,'require_login called without a service name!');
-	if (!isset($_SESSION['token']) || $_SESSION['token'] === null) redirect(getUrl('user','login?returnTo='.urlencode(location())));
-	$user = getLocallyFromToken();
-	if ($user === null) validateToken($service_name);
-	if ($user === null) redirect(getUrl('user','login?returnTo='.urlencode(location())));
-	if (isset($user->theme)) $theme = $user->theme;
-}
-
-function dialog($question,$options = array('YES'=>'?confirm=yes','NO'=>'index')){
-	$result = '<fieldset class="dialog">'.$question.'</br>';
-	foreach ($options as $text => $link){
-		$result .= '<a class="button" href="'.$link.'">'.$text.'</a>&nbsp';
-	}
-	return $result.'</fieldset>';
-}
-
-function t($text,$replacements=null){
-	global $lang;
-	$lang_file ='lang.'.$lang.'.php';
-	if (file_exists($lang_file)){
-		include $lang_file;
-		if (isset($translations[$text])) $text=$translations[$text];
-	}
-	return replace_text($text,$replacements);
-}
-
-function init(){
-	global $user;
-	$user = null;
-	session_start();
-	if ($token_param = param('token')) $_SESSION['token'] = $token_param;
-	if (isset($_GET['token'])) redirect(location('token')); // if token was appended to url: set cookie and reload
-}
-
-function objectFrom($entity){
-	if (is_array($entity)){
-		$obj = new stdClass();
-		foreach ($entity as $key => $val) $obj->$key = objectFrom($val);
-		return $obj;
-	}
-	return $entity;
-}
-
-function conclude_vcard($vcard){
-	$result = '';	
-	if (isset($vcard->FN)) $result = trim($vcard->FN);
-	if ($result != '') return $result;
-	if (isset($vcard->N)) $result = trim($vcard->N->given.' '.$vcard->N->family);
-	if ($result != '') return $result;
-	debug('error in conclude_vcard: no name set',true);
-}
-
-function address_from_vcard($vcard){
-	$result = '';
-	if (isset($vcard->FN)) $result .= $vcard->FN."\n";
-	if (isset($vcard->N)) $result .= trim($vcard->N->given.' '.$vcard->N->family) . "\n";
-	$address = $vcard->ADR;
-	if (is_array($address)) $address = reset($address);
-	if (!isset($address->post_box)) $result .= t('Postbox: ?',$address->post_box)."\n"; // Postfach
-	if (!empty($address->ext_addr)) $result .= $address->ext_addr."\n";				// Adresszusatz
-	if (!empty($address->street)) $result .=	$address->street."\n";				// Straße
-	if (!empty($address->post_code)) $result .=	$address->post_code." ";				// Postleitzahl
-	if (!empty($address->locality)) $result .=	$address->locality."\n";				// Ort
-	if (!empty($address->region)) $result .=	$address->region." / ";				// Region
-	if (!empty($address->country)) $result .=	$address->country;				// Land
-
-	return $result;
-}
-
-function html2plain($text){
-	$text = str_replace(['<br/>','<br />','<br>'],"\n",$text);
-	$text = str_replace(['<li>'],"- ",$text);
-	$text = str_replace(['</li>','<ul>','</ul>'],'',$text);
-	$text = str_replace('"','""',$text);
-	return $text;
-}
-
-function module_version(){
-	if (!defined('MODULE')) return '';
-	if (empty($_SESSION['modules'])) $_SESSION['modules'] = [];
-	if (!isset($_SESSION['modules'][MODULE])) $_SESSION['modules'][MODULE] = exec('git show -s --format=%ci HEAD');
-	return '"'.MODULE.'" module ('.$_SESSION['modules'][MODULE].') - ';
+function warn($message,$args = null){
+	if ($message === null) return;
+	$_SESSION['warnings'][crc32($message)] = t($message,$args);
 }
 
 class UmbrellaObject{
