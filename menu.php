b2e18fbb22a0c7abe2650fd0352d880ba064e672
Stephan Richter <github@keawe.de>
2018-10-02 10:19:33 +0200

    updated icon

--- menu.php
+++ menu.php
@@ -1,6 +1,7 @@
 <?php $base = getUrl('user'); ?>
-<a class="symbol" title="<?= t('add user')?>" href="<?= $base ?>add"></a>
-<a class="symbol" title="<?= t('connect with other account')?>" href="<?= $base ?>add_openid_login"></a>
 <?php if (isset($user->id)) { ?>
 <a class="symbol" title="<?= t('edit your account')?>" href="<?= $base . $user->id ?>/edit"></a>
 <?php } ?>
+<a class="symbol" title="<?= t('add user')?>" href="<?= $base ?>add"></a>
+<a class="symbol" title="<?= t('connect with other account')?>" href="<?= $base ?>add_openid_login"></a>
+
