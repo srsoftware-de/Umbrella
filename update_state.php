0d12b1a6a1874f05921ebed7ca1945381cb3c7eb
Stephan Richter <github@keawe.de>
2018-10-02 14:35:08 +0200

    re-implemented function

--- update_state.php
+++ update_state.php
@@ -1,21 +1,28 @@
 <?php include 'controller.php';
-
 require_login('time');
 
-$time = Timetrack::load(['ids'=>param(id)]);
-
-if ($new_state = param('state')){
-	switch ($new_state){
-		case 'open':
-			$time->patch(['state'=>TIME_STATUS_OPEN]); break;
-		case 'pending':
-			$time->patch(['state'=>TIME_STATUS_PENDING]); break;
-		case 'complete':
-			$time->patch(['state'=>TIME_STATUS_COMPLETE]); break;
+if ($time_id = param('id')){
+	$time = Timetrack::load(['ids'=>$time_id]);
+	
+	if ($new_state = param('state')){
+		switch ($new_state){
+			case 'open':
+				$time->patch(['state'=>TIME_STATUS_OPEN]); break;
+			case 'pending':
+				$time->patch(['state'=>TIME_STATUS_PENDING]); break;
+			case 'complete':
+				$time->patch(['state'=>TIME_STATUS_COMPLETE]); break;
+		}
+		$time->save();
 	}
-	$time->save();
+	$redirect = getUrl('time',$time_id.DS.'view');
 }
 
-if ($redirect = param('returnTo')){
-	redirect($redirect);
-} else redirect(getUrl('time',$this->id.'/view'));
+if ($pending_time_ids = param('PENDING')){ // used by document/view
+	$times = Timetrack::load(['ids'=>explode(',',$pending_time_ids)]);
+	foreach ($times as $time) $time->patch(['state'=>TIME_STATUS_PENDING])->save();
+	$redirect = getUrl('time');
+}
+
+redirect(param('returnTo',$redirect));
+
