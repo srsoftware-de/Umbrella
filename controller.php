ff663aa16f274069d646970d48a069c98d572dbd
Stephan Richter <github@keawe.de>
2018-12-03 11:10:36 +0100

    overhauled Note::load to enable search of notes of other users

--- controller.php
+++ controller.php
@@ -51,9 +51,20 @@ class Note extends UmbrellaObjectWithId{
 		global $user;
 		$db = get_or_create_db();
 
-		$sql = 'SELECT * FROM notes WHERE user_id = ?';
-		$args = [$user->id];
 		$single = false;
+		$user_dependent = true;
+		$where = [];
+		$args = [];
+
+		$sql = 'SELECT * FROM notes';
+
+		if (isset($options['key'])){
+			$key = '%'.$options['key'].'%';
+			$where[] = 'note LIKE ?';
+			$args = array_merge($args, [$key]);
+			$user_dependent = false;
+			$options['limit'] = -1;
+		}
 
 		if (isset($options['ids'])){
 			$ids = $options['ids'];
@@ -62,14 +73,8 @@ class Note extends UmbrellaObjectWithId{
 				$ids = [$ids];
 			}
 			$qMarks = str_repeat('?,', count($ids)-1).'?';
-			$sql .= ' AND id IN ('.$qMarks.')';
+			$where[] = 'id IN ('.$qMarks.')';
 			$args = array_merge($args, $ids);
-		}
-
-		if (isset($options['key'])){
-			$key = '%'.$options['key'].'%';
-			$sql .= ' AND note LIKE ?';
-			$args = array_merge($args, [$key]);
 		}
 
 		if (isset($options['uri'])){
@@ -84,7 +89,7 @@ class Note extends UmbrellaObjectWithId{
 				case 'model': // uris are of the form model:project:<project id> or model:<model id>
 					if (strpos($id, 'project:')===0){ // in the first case: request project
 						$parts = explode(':', $id);
-						$entities = request('project','json',['ids'=>$array_pop($parts)]);
+						$entities = request('project','json',['ids'=>array_pop($parts)]);
 						if (empty($entities)) return [];
 						break;
 					}
@@ -95,9 +100,17 @@ class Note extends UmbrellaObjectWithId{
 					if (empty($entities)) return [];
 					break;
 			}
-			$sql = 'SELECT * FROM notes WHERE uri = ?';
-			$args = [$uri];
+			$where[] = 'uri = ?';
+			$args = array_merge($args,[$uri]);
+			$user_dependent = false;
 		}
+
+		if ($user_dependent){
+			$where[] = 'user_id = ?';
+			$args[] = $user->id;
+		}
+
+		if (!empty($where)) $sql .= ' WHERE '.implode(' AND ', $where);
 
 		$order = isset($options['order']) ? $options['order'] : 'di';
 
@@ -126,7 +139,126 @@ class Note extends UmbrellaObjectWithId{
 			if ($single) return $note;
 			$notes[$id] = $note;
 		}
+
 		if ($single) return null; // no note found, which implies the loop is skipped
+
+		if (isset($options['key'])){
+			// this does a user-independent search. now we have to filter out those notes, to which the user has no access
+			$docs = [];
+			$files = [];
+			$models = [];
+			$project_ids = [];
+			$stock_items = [];
+			$task_ids = [];
+
+			$companies = request('company','json');
+			$accessible_company_ids = empty($companies) ? [] : array_keys($companies);
+			unset($companies);
+
+			$projects = request('project','json');
+			$accessible_project_ids = empty($projects) ? [] : array_keys($projects);
+			unset($projects);
+
+			$accessible_uris = [];
+
+			foreach ($notes as $note_id => $note){
+				$uri_parts = explode(':', $note->uri);
+				$realm = array_shift($uri_parts);
+				$id = implode(':', $uri_parts);;
+				switch($realm){
+					case 'document':
+						$docs[$id] = true;
+						break;
+					case 'files':
+						$files[$id] = true;
+						break;
+					case 'model':
+						$models[$id] = true;
+						break;
+					case 'project':
+						$project_ids[$id]=true;
+						break;
+					case 'stock':
+						$stock_items[$id]=true;
+						break;
+					case 'task':
+						$task_ids[$id]=true;
+						break;
+					default:
+						debug(['realm'=>$realm,implode(':', $uri_parts)]);
+				}
+			}
+
+			if (!empty($docs)){
+				$docs = request('document','json',['ids'=>array_keys($docs)]);
+				foreach ($docs as $id => $dummy) $accessible_uris[] = 'document:'.$id;
+			}
+
+			if (!empty($files)){
+				foreach ($files as $path => $dummy){
+					$parts = explode(DS, $path);
+					$realm = array_shift($parts);
+					$id = array_shift($parts);
+					switch ($realm){
+						case 'company':
+							if (in_array($id, $accessible_company_ids)) $accessible_uris[] = 'files:'.$realm.DS.$id.DS.implode(DS, $parts);
+							break;
+						case 'project':
+							if (in_array($id, $accessible_project_ids)) $accessible_uris[] = 'files:'.$realm.DS.$id.DS.implode(DS, $parts);
+							break;
+						case 'user':
+							if ($id == $user->id) $accessible_uris[] = 'files:'.$realm.DS.$id.DS.implode(DS, $parts);
+							break;
+					}
+				}
+			}
+
+			if (!empty($models)){
+				$mids = [];
+				foreach ($models as $model_id => $dummy){
+					if (strpos($model_id,'project:')===0){
+						$pid = substr($model_id,8);
+						if (in_array($pid, $accessible_project_ids)) $accessible_uris[] = 'model:'.$model_id;
+					} else $mids[$model_id] = true;
+				}
+				if (!empty($mids)){
+					$models = request('model','json',['ids'=>array_keys($mids)]);
+					foreach ($models as $mid => $dummy) $accessible_uris[] = 'model:'.$mid;
+				}
+			}
+
+			if (!empty($project_ids)){
+				foreach ($project_ids as $pid => $dummy){
+					if (in_array($pid, $accessible_project_ids)) $accessible_uris[] = 'project:'.$pid;
+				}
+			}
+
+			if (!empty($stock_items)){
+				foreach ($stock_items as $item_id => $dummy){
+					$parts = explode(':', $item_id);
+					$realm = array_shift($parts);
+					$id = array_shift($parts);
+					switch ($realm){
+						case 'company':
+							if (in_array($id, $accessible_company_ids)) $accessible_uris[] = 'stock:'.$item_id;
+							break;
+						case 'user':
+							if ($id == $user->id) $accessible_uris[] = 'stock:'.$item_id;
+							break;
+					}
+				}
+			}
+
+			if (!empty($task_ids)){
+				$tasks = request('task','json',['ids'=>array_keys($task_ids)]);
+				foreach ($tasks as $tid => $dummy) $accessible_uris[] = 'task:'.$tid;
+			}
+
+			foreach ($notes as $id => $note){
+				if (!in_array($note->uri, $accessible_uris))	unset($notes[$id]);
+			}
+		}
+
 		return $notes;
 	}
 
@@ -153,11 +285,11 @@ class Note extends UmbrellaObjectWithId{
 		$this->uri = $uri;
 		$this->note = $note;
 	}
-	
+
 	function save(){
 		global $user;
 		$db = get_or_create_db();
-		
+
 		if (isset($this->id)){
 			if (!empty($this->dirty)){
 				$sql = 'UPDATE notes SET';
@@ -169,7 +301,7 @@ class Note extends UmbrellaObjectWithId{
 				$sql = rtrim($sql,',').' WHERE id = :id';
 				$query = $db->prepare($sql);
 				assert($query->execute($args),'Was no able to update note in database!');
-			}			
+			}
 		} else {
 			$sql = 'INSERT INTO notes (user_id, uri, note, timestamp) VALUES (:uid, :uri, :note, :time);';
 			$args = [':uid'=>$user->id, ':uri'=>$this->uri, ':note'=>$this->note, ':time'=>time()];
