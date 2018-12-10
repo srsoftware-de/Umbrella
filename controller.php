da372bb302299177702b8d4fd32b93f59cd94208
Stephan Richter <github@keawe.de>
2018-12-10 17:41:00 +0100

    bugfix

--- controller.php
+++ controller.php
@@ -2,15 +2,15 @@
 
 	const TIME_PERMISSION_OWNER = 1;
 	const TIME_PERMISSION_PARTICIPANT = 2;
-	
+
 	const MODULE = 'Time';
-	
+
 	const TIME_STATUS_STARTED = 10; // time tracking started
 	const TIME_STATUS_OPEN = 20; // time tracking concluded. not invoiced
 	const TIME_STATUS_PENDING = 40; // time track used in unsent invoice
 	const TIME_STATUS_COMPLETE = 60; // time track used in sent invoice
 	const TIME_STATUS_CANCELED = 100;
-	
+
 	const TIME_STATES = [TIME_STATUS_CANCELED => 'canceled',
 						 TIME_STATUS_PENDING => 'pending',
 						 TIME_STATUS_OPEN => 'open',
@@ -18,21 +18,21 @@
 						 TIME_STATUS_STARTED => 'started'
 						];
 	$TIME_PERMISSIONS = array(TIME_PERMISSION_OWNER=>'owener',TIME_PERMISSION_PARTICIPANT=>'participant');
-	
+
 	$title = t('Umbrella Timetracking');
-	
+
 	function get_or_create_db(){
 		$table_filename = 'times.db';
 		if (!file_exists('db')) assert(mkdir('db'),'Failed to create time/db directory!');
 		assert(is_writable('db'),'Directory time/db not writable!');
 		if (!file_exists('db/'.$table_filename)){
 			$db = new PDO('sqlite:db/'.$table_filename);
-	
+
 			$tables = [
 				'times'=>Timetrack::table(),
 				'task_times'=>Timetrack::task_table(),
 			];
-	
+
 			foreach ($tables as $table => $fields){
 				$sql = 'CREATE TABLE '.$table.' ( ';
 				foreach ($fields as $field => $props){
@@ -67,16 +67,16 @@
 		}
 		return $db;
 	}
-	
+
 	class Timetrack extends UmbrellaObjectWithId{
 		static function startNew(){
 			global $user;
 			return (new Timetrack())->patch(['user_id'=>$user->id,'subject'=>t('started timetrack'),'start_time'=>time(),'state'=>TIME_STATUS_STARTED])->save();
 		}
-		
+
 		static function table(){
 			return [
-				'id'				=> ['INTEGERR','KEY'=>'PRIMARY'],
+				'id'				=> ['INTEGER','KEY'=>'PRIMARY'],
 				'user_id'			=> ['INT','NOT NULL'],
 				'subject'			=> ['VARCHAR'=>255,'NOT NULL'],
 				'description'		=> 'TEXT',
@@ -85,7 +85,7 @@
 				'state'				=> ['INT','NOT NULL','DEFAULT 10'],
 			];
 		}
-		
+
 		static function task_table(){
 			return [
 				'task_id'		=> ['INT','NOT NULL'],
@@ -93,7 +93,7 @@
 				'PRIMARY KEY'	=> '(task_id, time_id)',
 			];
 		}
-		
+
 		static function load($options){
 			global $parsedown, $user;
 
@@ -101,12 +101,12 @@
 			$where = [];
 			$args = [];
 			$single = false;
-			
+
 			if (empty($options['task_ids'])) {
 				$where[] = 'user_id = ?';
 				$args[] = $user->id;
 			}
-			
+
 			if (isset($options['ids'])){
 				if (!is_array($options['ids'])) {
 					$single = true;
@@ -116,17 +116,17 @@
 				$where[] = 'id IN ('.$qMarks.')';
 				$args = array_merge($args, $options['ids']);
 			}
-			
+
 			if (isset($options['search'])){
 				$key = '%'.$options['search'].'%';
 				$where[] = '(subject LIKE ? OR description LIKE ?)';
 				$args = array_merge($args,[$key,$key]);
 			}
-			
+
 			if (isset($options['open']) && $options['open']){
 				$where[] = 'end_time IS NULL';
 			}
-		
+
 			if (!empty($options['task_ids'])){
 				$ids = $options['task_ids'];
 				if (!is_array($ids)) $ids = [$ids];
@@ -134,9 +134,9 @@
 				$where[] = 'id IN (SELECT time_id FROM task_times WHERE task_id IN ('.$qMarks.'))';
 				$args = array_merge($args, $ids);
 			}
-			
+
 			if (!empty($where)) $sql .= ' WHERE '.implode(' AND ', $where);
-			
+
 			if (isset($options['order'])){
 				switch ($options['order']){
 					case 'description':
@@ -149,7 +149,7 @@
 			} else {
 				$sql .= ' ORDER BY state ASC, end_time DESC';
 			}
-		
+
 			/* Fetch times from time db. Collect task ids of times */
 			$db = get_or_create_db();
 			//debug(query_insert($sql, $args),1);
@@ -157,7 +157,7 @@
 			//debug($query,1);
 			assert($query->execute($args),'Was not able to load times!');
 			$rows = $query->fetchAll(INDEX_FETCH);
-			
+
 			$all_times = [];
 			$task_ids = [];
 			foreach ($rows as $row){
@@ -171,11 +171,11 @@
 
 			/* Fetch tasks referenced by times */
 			$referenced_tasks = request('task','json',['ids'=>$task_ids]);
-			
+
 
 			/* Fetch all projects and filter by referencing takss */
 			$projects = isset($options['project_id']) ? request('project','json',['ids'=>$options['project_id']]) : request('project','json');
-			
+
 			/* assign projects to tasks, thereby filter tasks:
 			 * only task which belong to a loaded projects will be kept */
 			$filtered_tasks = [];
@@ -186,7 +186,7 @@
 					$filtered_tasks[$tid] = $task;
 				}
 			}
-			
+
 			/* assign tasks to times, thereby filter times:
 			 * only times, which have at least one task in the list of filtered tasks are kept.
 			 * This ensures, in turn, that only tasks of the selected project(s) are shown.
@@ -204,7 +204,7 @@
 			if ($single) return null;
 			return $filtered_times;
 		}
-		
+
 		function assign_task($task = null){
 			if (isset($this->tasks[$task['id']])){
 				info('The task "?" already was assigned to this timetrack.',$task['name']);
@@ -215,7 +215,7 @@
 			}
 			return $this;
 		}
-		
+
 		function delete(){
 			$db = get_or_create_db();
 			$query = $db->prepare('DELETE FROM times WHERE id = :tid');
@@ -224,7 +224,7 @@
 			assert($query->execute(array(':tid'=>$this->id)),'Was not able to drop task_time entry!');
 			unset($this->id);
 		}
-		
+
 		function save(){
 			global $user,$services;
 			$db = get_or_create_db();
@@ -251,25 +251,25 @@
 						$fields[]=$f;
 						$args[':'.$f] = $this->{$f};
 					}
-				}			
+				}
 				$sql = 'INSERT INTO times ( '.implode(', ',$fields).' ) VALUES ( :'.implode(', :',$fields).' )';
 				$query = $db->prepare($sql);
 				//debug(query_insert($query, $args),1);
 				assert($query->execute($args),'Was not able to insert new timetrack');
 				$this->id = $db->lastInsertId();
 			}
-			
+
 			$query = $db->prepare('INSERT OR IGNORE INTO task_times (task_id, time_id) VALUES (:task, :time)');
 			foreach ($this->tasks as $task_id => $task) assert($query->execute(array(':task'=>$task_id,':time'=>$this->id)),'Was not able to assign task to timetrack.');
-			
+
 			return $this;
 		}
-		
+
 		function state(){
 			$t = TIME_STATES;
 			return $t[$this->state];
 		}
-		
+
 		function tasks(){
 			if (empty($this->tasks)) $this->task_ids();
 			$ids_of_missing_tasks = [];
@@ -283,7 +283,7 @@
 			}
 			return $this->tasks;
 		}
-		
+
 		function task_ids(){
 			if (empty($this->tasks)) {
 				$sql = 'SELECT task_id FROM task_times WHERE time_id = :tid';
@@ -295,12 +295,12 @@
 			}
 			return array_keys($this->tasks);
 		}
-		
+
 		function update($subject = null,$description = null,$start = null,$end = null,$state = TIME_STATUS_OPEN){
 			assert($subject !== null,'Subject must not be null!');
 			$start_time = strtotime($start);
 			assert($start_time !== false,'Invalid start time passed to time.update!');
-	
+
 			$end_time = strtotime($end);
 			if (!$end_time) $end_time = null;
 			if ($end_time === null) $state = TIME_STATUS_STARTED;
@@ -314,7 +314,7 @@
 			return str_replace("\n", '<br/>', $text);
 		}
 	}
-	
+
 	$parsedown = null;
 	if (file_exists('../lib/parsedown/Parsedown.php')){
 		include '../lib/parsedown/Parsedown.php';
