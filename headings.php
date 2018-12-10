a9748419d5291954bb485859b54e4aebf34cd89c
Stephan Richter <github@keawe.de>
2018-12-10 16:25:43 +0100

    bookmarks now using keywords from target site

--- headings.php
+++ headings.php
@@ -4,16 +4,21 @@ include '../bootstrap.php';
 if ($page = param('page')){
 	$xml = new DOMDocument();
 	@$xml->loadHTMLFile($page);
-	
+
 	$headings = [];
 	foreach (['title','h1','h2','h3'] as $tag){
 		foreach ($xml->getElementsByTagName($tag) as $head){
 			$txt = trim($head->textContent);
 			if (strpos($txt,"\n")===false) $headings[]=$txt;
 		}
-		if (!empty($headings)) break;
 	}
-	die(json_encode($headings));
+
+	$keywords = [];
+	foreach ($xml->getElementsByTagName('meta') as $tag){
+		if ($tag->getAttribute('name') != 'keywords') continue;
+		$keywords = explode(',', str_replace(' ','_',str_replace(', ',',',$tag->getAttribute('content'))));
+	}
+	die(json_encode(['headings'=>$headings,'keywords'=>$keywords],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE));
 } else {
 	echo 'No path set!';
 }
