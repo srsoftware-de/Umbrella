91432a8e8104d9c28adeb5e25ff8c00c39a82940
Stephan Richter <github@keawe.de>
2018-12-21 11:01:19 +0100

    extended tests for project/add

--- tests/02-project-add-test.py
+++ tests/02-project-add-test.py
@@ -39,12 +39,34 @@ expect('<textarea name="description"></textarea>' in r.text)
 expect('<input name="tags" type="text" value="" />' in r.text)
 expect('<button type="submit">' in r.text)
 
-# check the form
+# form should not create new project as long as name is missing, but it should keep entered data
 r = session.post('http://localhost/project/add',data={'description':'this is the description','tags':'tag1 tag2'})
 expect('<form method="POST">' in r.text)
 expect('<input type="text" name="name" />' in r.text)
 expect('<textarea name="description">this is the description</textarea>' in r.text)
 expect('<input name="tags" type="text" value="tag1 tag2" />' in r.text)
 expect('<button type="submit">' in r.text)
+
+db = sqlite3.connect('../db/projects.db')
+cursor = db.cursor()
+
+# check no projects exists
+cursor.execute('SELECT * FROM projects')
+rows = cursor.fetchall()
+expect(not rows)
+
+# project should have been created, user should be redirected to index
+r = session.post('http://localhost/project/add',data={'name':'testproject','description':'this is the description','tags':'tag1 tag2'},allow_redirects=False)
+expectRedirect(r,'http://localhost/project/1/view')
+cursor.execute('SELECT * FROM projects')
+expect(cursor.fetchall() == [(1, None, 'testproject', 'this is the description', 10)])
+
+# project should appear on index page
+r = session.get('http://localhost/project/',allow_redirects=False)
+expect('<body class="project">' in r.text)
+expect('<table class="project-index">' in r.text)
+expect('<td><a href="1/view">testproject</a></td>'in r.text)
+expect('<td>offen</td>' in r.text)
+expect('admin<br/>' in r.text)
 
 print ('done')
