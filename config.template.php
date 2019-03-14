92f88046f85b64366dfd894066230aca631c5f0e
Stephan Richter <github@keawe.de>
2019-03-14 11:49:13 +0100

    added poll, minor style corrections

--- config.template.php
+++ config.template.php
@@ -9,11 +9,12 @@ $services['time']		=array('name'=>'Timetrack',	'path'=>'https://umbrella.keawe.d
 $services['invoice']	=array('name'=>'Invoices',	'path'=>'https://umbrella.keawe.de/invoice/');
 $services['document']   = [ 'name' => 'Documents',	'path'=>'https://umbrella.keawe.de/document/'];
 $services['items']  	=array('name'=>'Items',		'path'=>'https://umbrella.keawe.de/item/');
-$services['contact']	=array('name'=>'Contacts',	'path'=>'https://umbrella.keawe.de/contact/');
-$services['bookmark']	=array('name'=>'Bookmarks',	'path'=>'https://umbrella.keawe.de/bookmark/');
-$services['files']		=array('name'=>'Files',		'path'=>'https://umbrella.keawe.de/files/');
+$services['contact']	= ['name'=>'Contacts',	'path'=>'https://umbrella.keawe.de/contact/'];
+$services['bookmark']	= ['name'=>'Bookmarks',	'path'=>'https://umbrella.keawe.de/bookmark/'];
+$services['files']		= ['name'=>'Files',		'path'=>'https://umbrella.keawe.de/files/'];
 $services['notes']	    = ['name'=>'Notes', 	'path'=>'https://umbrella.keawe.de/notes/'];
 $services['model']      = ['name'=>'Models',	'path'=>'https://umbrella.keawe.de/model/'];
+$services['poll']       = ['name'=>'Polls',     'path'=>'https://umbrella.keawe.de/poll/'];
 $services['stock']      = ['name'=>'Stock',     'path'=>'https://umbrella.keawe.de/stock/'];
 $lang = 'de';
 ?>
