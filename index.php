b51f2c62045bfb951d5f90d1f0658286524da909
Stephan Richter <github@keawe.de>
2019-05-21 17:08:51 +0200

    added messages to user index, removed separate messages page

--- index.php
+++ index.php
@@ -14,13 +14,46 @@ if ($service_data  = param('login_service')){
 	$service->patch($service_data)->save();
 }
 if ($login_service_name = param('delete_login_service')) LoginService::load($login_service_name)->delete();
-include '../common_templates/head.php';
+$limit = param('limit');
+if (empty($limit)){
+	$messages = Message::load(['since'=>$user->last_logoff]);
+} else $messages = Message::load(['limit'=>$limit]);
 
+include '../common_templates/head.php';
 include '../common_templates/main_menu.php';
 include 'menu.php';
-include '../common_templates/messages.php';
-
-
+include '../common_templates/messages.php'; ?>
+<fieldset>
+	<legend>
+		<?= empty($limit)?t('Messages since last logoff'):t('Last ◊ messages',$limit)?>
+	</legend>
+	<table class="messages">
+		<tr>
+			<th><?= t('Created')?></th>
+			<th><?= t('From')?></th>
+			<th><?= t('Subject')?></th>
+			<th><?= t('Text')?></th>
+			<th><?= t('State')?></th>
+		</tr>
+		<?php foreach ($messages as $message){ ?>
+		<tr>
+			<td><?= date('Y-m-d / H:i:s',$message->timestamp) ?></td>
+			<td><?= $message->from->login ?></td>
+			<td><?= $message->subject ?></td>
+			<td><?= markdown($message->body) ?></td>
+			<td>
+				<?php switch ($message->state){
+					case Message::SENT: echo t('Already sent via mail.'); break;
+					case Message::WAITING: echo t('Waiting to be sent by mail.'); break;
+					default: echo t('Unknown message state');
+				}
+				?>
+			</td>
+		</tr>
+		<?php } ?>
+	</table>
+</fieldset>
+<?php
 
 if (isset($services['contact'])){
 	$contact = request('contact','json',['assgined'=>true]);
--- notify.php
+++ notify.php
@@ -8,7 +8,7 @@ if (empty($subject)) error('No subject passed to notify!');
 $body = post('body'); // text
 if (empty($body)) error('No message body passed to notify!');
 
-$recipients = post('recipients'); // user id
+$recipients = post('recipients'); // user id(s)
 if (empty($recipients)) error('No recipient(s) passed to notify!');
 
 if (no_error()) {
@@ -18,24 +18,3 @@ if (no_error()) {
 	$message = new Message();
 	$message->patch(['author'=>$user->id,'timestamp'=>time(),'subject'=>$subject,'body'=>$body])->deliverTo($users);
 }
-
-include '../common_templates/head.php';
-include '../common_templates/main_menu.php';
-include 'menu.php';
-include '../common_templates/messages.php';
-
-if (!empty($message)) debug($message);
-?>
-
-<form method="POST">
-	Subject: <input type="text" name="subject" value="" /><br/>
-	Message: <input type="text" name="body" value="" /><br/>
-	Recipients: <div>
-		<input type="checkbox" name="recipients[]" value="2" checked="checked"/>
-	</div>
-	<input type="submit"/>
-</form>
-
-<?php
-
-include '../common_templates/closure.php';
