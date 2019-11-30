b34b087bff5f40b63a96051d33b7ab2840deaba4
Stephan Richter <github@keawe.de>
2019-11-30 15:37:54 +0100

    now allowing order parameter in jspn.php

--- json.php
+++ json.php
@@ -6,8 +6,9 @@ $options = [];
 if ($ids = param('ids'))$options['ids'] = explode(',',$ids);
 if ($task_ids = param('task_ids')) $options['task_ids'] = $task_ids;
 if ($ids_only = param('ids_only')) $options['ids_only'] = $ids_only;
+if ($order = param('order')) $options['order'] = $order;
 
 $times = Timetrack::load($options);
-//foreach ($times as &$time) unset($time->tasks); // needed for document/view   
+//foreach ($times as &$time) unset($time->tasks); // needed for document/view
 
 die(json_encode($times));
