1e0a1dd7a005004f5600d82264985c0059419807
Stephan Richter <github@keawe.de>
2021-02-24 09:03:34 +0100

    bugfix: no menue.php present

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ require_login('mindmap');
 
 include '../common_templates/head.php'; 
 include '../common_templates/main_menu.php';
-include 'menu.php';
+#include 'menu.php';
 include '../common_templates/messages.php';  ?> INDEX <?php 
 include '../common_templates/closure.php'; ?>
