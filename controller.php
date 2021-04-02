5f4a77962218c384cef0b82d2a82ed627f44f665
Stephan Richter <github@keawe.de>
2021-04-02 19:29:06 +0200

    Security Bugfix

--- controller.php
+++ controller.php
@@ -13,10 +13,10 @@
 
 	function get_or_create_db(){
 		$table_filename = 'stock.db';
-		if (!file_exists('db') && !mkdir('db')) throw new Exception('Failed to create '.strtolower(MODULE).'/db directory!');
-		if (!is_writable('db')) throw new Exception('Directory '.strtolower(MODULE).'/db not writable!');
-		if (!file_exists('db/'.$table_filename)){
-			$db = new PDO('sqlite:db/'.$table_filename);
+		if (!file_exists('.db') && !mkdir('.db')) throw new Exception('Failed to create '.strtolower(MODULE).'/.db directory!');
+		if (!is_writable('.db')) throw new Exception('Directory '.strtolower(MODULE).'/.db not writable!');
+		if (!file_exists('.db/'.$table_filename)){
+			$db = new PDO('sqlite:.db/'.$table_filename);
 
 			$tables = [
 				'items'=>Item::table(),
@@ -55,7 +55,7 @@
 				if (!$db->query($sql)) throw new Exception('Was not able to create '.$table.' table in '.$table_filename.' ("'.$sql.'") !');
 			}
 		} else {
-			$db = new PDO('sqlite:db/'.$table_filename);
+			$db = new PDO('sqlite:.db/'.$table_filename);
 		}
 		return $db;
 	}
