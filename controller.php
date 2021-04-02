9257c58d0701bbccec6a2353dd13dbfe9f15f992
Stephan Richter <github@keawe.de>
2021-04-02 19:28:03 +0200

    Security Bugfix

--- controller.php
+++ controller.php
@@ -5,10 +5,10 @@ $title = 'Umbrella Real Time Chat';
 
 function get_or_create_db(){
 	$table_filename = 'rtc.db';
-	if (!file_exists('db') && !mkdir('db')) throw new Exception('Failed to create rtp/db directory!');
-	if (!is_writable('db')) throw new Exception('Directory rtc/db not writable!');
-	if (!file_exists('db/'.$table_filename)){
-		$db = new PDO('sqlite:db/'.$table_filename);
+	if (!file_exists('.db') && !mkdir('.db')) throw new Exception('Failed to create rtp/.db directory!');
+	if (!is_writable('.db')) throw new Exception('Directory rtc/.db not writable!');
+	if (!file_exists('.db/'.$table_filename)){
+		$db = new PDO('sqlite:.db/'.$table_filename);
 
 		$tables = [
 			'channels'=>Channel::table(),
@@ -44,7 +44,7 @@ function get_or_create_db(){
 			if (!$db->query($sql)) throw new Exception('Was not able to create '.$table.' table in '.$table_filename.'!');
 		}
 	} else {
-		$db = new PDO('sqlite:db/'.$table_filename);
+		$db = new PDO('sqlite:.db/'.$table_filename);
 	}
 	return $db;
 }
