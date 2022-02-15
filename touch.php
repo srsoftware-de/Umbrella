02dd39b62f84e42a657ba20137640e17d856fe7f
Stephan Richter <s.richter@srsoftware.de>
2022-02-15 23:01:15 +0100

    bugfix

--- touch.php
+++ touch.php
@@ -4,4 +4,4 @@ require_login('notes');
 
 $note = Note::load(['ids' => param('id')])->patch(['timestamp'=>time()])->save(); // update timestamp
 
-redirect($note->url()); // redirect to referencing object
+redirect(url($note->uri)); // redirect to referencing object
